0780cc12789ed8cd0f55fa0e9d117a4ca79d1eb3
pkapse173 <55979904+pkapse173@users.noreply.github.com>
2021-08-05 18:54:03 +0530

    Add files via upload

---
+++ Node-Sessions/Day 4 Training/app.js
@@ -0,0 +1,10 @@
+var log = {
+
+    info : function(info){
+        console.log('info ' + info);
+    },
+    warning : function(warning){
+        console.log('warning ' + warning);
+    }
+};
+module.exports = log;
---
+++ Node-Sessions/Day 4 Training/log.js
@@ -0,0 +1,4 @@
+var mylog = require("./app");
+
+mylog.info("You are in info department");
+mylog.warning("Tour are in warning department");
