7e769c34df99d1714b8caa0072ebe7060b0f90f0
pkapse173 <55979904+pkapse173@users.noreply.github.com>
2021-08-05 18:56:33 +0530

    Add files via upload

---
+++ Node-Sessions/Day 4 Training/index.js
@@ -0,0 +1,9 @@
+console.log("Hello from Node.js");
+
+var obj = {
+    authorname : 'Pranav',
+    age : 23,
+    city : 'Pune',
+    language : 'Javascript with Node.js'
+}
+console.log(obj.authorname,obj.city,obj.language);
